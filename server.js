62199e226c4033a6c5d73261e7bbeb8185748b3d
goldkravi <30519233+goldkravi@users.noreply.github.com>
2017-08-08 14:23:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/ar',function(req,res){
+    res.send('Hai article 1');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
